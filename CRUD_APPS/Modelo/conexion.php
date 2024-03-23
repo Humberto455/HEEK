5e84bd645f9775e27faa692737cc34cfbc806c48
Humberto455 <hrasgado3892@ppsc.edu.mx>
2024-03-22 22:19:30 -0600

    Proyecto v1

---
+++ CRUD_APPS/Modelo/conexion.php
@@ -0,0 +1,4 @@
+<?php
+$conexion = new mysqli("db","mariadb","mariadb","mariadb");
+$conexion -> set_charset("utf8");
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------*/
+?>
+
+<html>
+	<head>
+		<title>Visual Studio Code Remote :: PHP</title>
+	</head>
+	<body>
+		<?php 
+			echo '<script>window.location="./CRUD_APPS/home.php"</script>';
+		?>
+	</body>
+</html>
